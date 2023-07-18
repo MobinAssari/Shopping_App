3ade31198e16efd869c02f7727fa9193cace347d
Mobin <mobin.aranii@gmail.com>
2023-07-18 20:48:22 +0330

    new item

---
+++ lib/lib/widgets/new_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class NewItem extends StatefulWidget{
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build( context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
